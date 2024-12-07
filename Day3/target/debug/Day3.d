9c3affa0969716f4049359ab6c614df7b307c0db
naybyal <officiallynabiel@gmail.com>
2024-12-07 17:21:43 +0530

    Day 3: Mull It Over

--- Day3/target/debug/Day3.d
+++
@@ -1,1 +0,0 @@
-/home/nabiel/Documents/Code/advent-of-code/2024/Day3/target/debug/Day3: /home/nabiel/Documents/Code/advent-of-code/2024/Day3/main.rs
--- Day3/target/debug/deps/Day3-d23bb06f22bfe557.d
+++
@@ -1,5 +0,0 @@
-/home/nabiel/Documents/Code/advent-of-code/2024/Day3/target/debug/deps/Day3-d23bb06f22bfe557: main.rs
-
-/home/nabiel/Documents/Code/advent-of-code/2024/Day3/target/debug/deps/Day3-d23bb06f22bfe557.d: main.rs
-
-main.rs:
